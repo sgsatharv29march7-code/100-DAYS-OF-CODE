91569bfec8509662bf70888d637e4a6ea310449a
atharvsharma <sgs.atharv.29march.7@gmail.com>
2026-09-13 01:49:17 +0530

    new program

--- P8.c
+++ P8.c
@@ -3,12 +3,12 @@
 
 int main(){
     int n,i;
-    int sum;
+    int sum=0;
     printf("enter a number");
     scanf("%d",&n);
     for(i=1;i<=n;i++){
         sum=sum+i;
 
     }
-    printf("sum of first n natural numbers =%d",sum);
+    printf("sum of first n natural numbers = %d",sum);
 }
