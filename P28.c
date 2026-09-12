adee91ba7f2ff4bf50aa58b176a1378ab033783c
atharvsharma <sgs.atharv.29march.7@gmail.com>
2026-09-13 03:25:13 +0530

    new program

---
+++ P28.c
@@ -0,0 +1,30 @@
+//Q28: Write a program to print the product of even numbers from 1 to n.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+Output 1:
+8 (2 * 4)
+
+Input 2:
+6
+Output 2:
+48 (2 * 4 * 6)
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    int n,i;
+    int pro=1;
+    printf("enter a number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        if(i%2==0){
+            pro=pro*i;
+        }
+    }
+    printf("%d",pro);
+}
