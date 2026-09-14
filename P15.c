11108d9ad345eb9861789225f7968fa0ef1d35e8
atharvsharma <sgs.atharv.29march.7@gmail.com>
2026-09-14 08:16:09 +0530

    new program

--- P15.c
+++ P15.c
@@ -2,5 +2,24 @@
 #include <stdio.h>
 
 int main(){
+    char ch;
+    printf("enter a character");
+    scanf("%c",&ch);
+    if(ch>='A' && ch<='Z'){
+        printf("character is uppercase alphabet %c",ch);
+    }
+    else{
+        if(ch>='a' && ch<='z'){
+            printf("character is lowercase alphabet %c",ch);
+        }
+        else{
+            if(ch>='0' && ch<='9'){
+                printf("character is digit %c",ch);
+            }
+            else{
+                printf("character is special character %c",ch);
+            }
+        }
+    }
     
 }
