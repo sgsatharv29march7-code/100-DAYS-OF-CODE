8226a501ea02b5ededffb8ace84c75ad642e0031
atharvsharma <sgs.atharv.29march.7@gmail.com>
2026-09-21 03:39:10 +0530

    new program

---
+++ P52.c
@@ -0,0 +1,45 @@
+//Q52: Write a program to print the following pattern:
+/*
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+
+*/
+
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
+
+#include <stdio.h>
+
+int main(){
+ int i,j=0;
+ for(i=1;i<=5;i++){
+    for(j=1;j<=i;j++){
+        printf("*\n");
+    }
+    printf("\n");
+ }
+}
